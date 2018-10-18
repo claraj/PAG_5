7e93a99ec87e9adeb731dd83a9376ea4188b932d
clara <clara@minneapolis.edu>
2018-10-18 14:48:46 -0500

    Dictionaries lab

--- lab_questions/author_book.py
+++ lab_questions/author_book.py
@@ -1,7 +1,8 @@
-
+# Homework Week 5, Dictionaries
 
 def main():
 
+    # Don't modify the main method.
     books = {'JK Rolling': 'Harry Potter',
              'Al Sweigart': 'Automate the Boring Stuff With Python',
              'Margaret Wise Brown': 'Goodnight Moon'}
@@ -13,7 +14,9 @@ def main():
             add_book(books)
         elif selection == 'view':
             view_books(books)
-        elif selecton == 'edit':
+        elif selection == 'find':
+            find_book(books)
+        elif selection == 'edit':
             edit_book(books)
         elif selection == 'del':
             delete_book(books)
@@ -25,12 +28,46 @@ def main():
 
 
 def menu():
+    # Don't modify this method.
     print('add: Add new book')
     print('view: View all books')
+    print('find: Find a title for an author')
     print('edit: edit the title for a book')
     print('del: delete a book')
     print('exit: quit program')
     print()
 
 
-# TODO write the add_book function here...
+# TODO write the add_book function here.
+# This function should ask for an author and a title, and add them to the dictionary.
+# If the author is already in the dictionary, print an error message to the user and don't modify the dictionary.
+# Your program should not crash.
+
+
+# TODO write the view_books function here. This function should print all of the authors and titles.
+# This function should not ask for any input.
+
+
+# TODO write the find_book function here.
+# This function should ask for an author.
+# If the author is found in the dictionary, print the title of their book.
+# If the author is not found, print 'Not Found'
+
+
+# TODO write the edit_books function here.
+# This function should ask for an author, and the new book title.
+# If the author is found in the dictionary, Modify the dictionary so the author's title is the new value.
+# If the author is not found, print the string 'Not Found' and don't modify the dictionary. Your program should not crash.
+
+
+# TODO write the delete_book function here.
+# This function should ask for an author. Delete the key-value pair for this author.
+# If the author is found in the dictionary, delete that author's key-value pair.
+# If the author is not found, print 'Not Found' and don't modify the dictionary. Your program should not crash.
+
+# Make sure you use the exact function names given!
+
+
+# Don't modify this either.
+if __name__ == '__main__':
+    main()
--- tests/test_author_book.py
+++ tests/test_author_book.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 from lab_questions import author_book
-from tests.print_util import print_calls_contain_output
+from tests.print_util import print_calls_contain_output, all_output
 
 
 class TestAuthorBook(TestCase):
@@ -24,12 +24,58 @@ class TestAuthorBook(TestCase):
 
 
     @patch('builtins.input')
+    @patch('builtins.print')
+    def test_edit_book_not_found(self, mock_print, mock_input):
+        mock_input.side_effect = ['c', 'asdsdfsdf']
+        example = {'a': 'aaa', 'b': 'bbb'}
+        update = {'a': 'aaa', 'b': 'bbb'}
+        author_book.edit_book(example)
+        self.assertEqual(update, example)
+        self.assertIn('Not Found', all_output(mock_print))
+
+
+    @patch('builtins.input')
+    @patch('builtins.print')
+    def test_find_book(self, mock_print, mock_input):
+        mock_input.side_effect = ['a']
+        example = {'a': 'aaa', 'b': 'bbb'}
+        update = {'a': 'aaa', 'b': 'bbb'}
+
+        author_book.find_book(example)
+        self.assertEqual(update, example)  # Not modified
+        self.assertTrue('aaa' in all_output(mock_print))
+
+
+    @patch('builtins.input')
+    @patch('builtins.print')
+    def test_find_book_not_found(self, mock_print, mock_input):
+        mock_input.side_effect = ['c']
+        example = {'a': 'aaa', 'b': 'bbb'}
+        update = {'a': 'aaa', 'b': 'bbb'}
+
+        author_book.find_book(example)
+        self.assertEqual(update, example)  # Not modified
+        self.assertTrue('Not Found' in all_output(mock_print))
+
+
+    @patch('builtins.input')
     def test_delete_book(self, mock_input):
         mock_input.side_effect = ['a']
         example = {'a': 'aaa', 'b': 'bbb'}
         update = {'b': 'bbb'}
         author_book.delete_book(example)
         self.assertEqual(update, example)
+
+
+    @patch('builtins.input')
+    @patch('builtins.print')
+    def test_delete_book_not_found(self, mock_print, mock_input):
+        mock_input.side_effect = ['c']
+        example = {'a': 'aaa', 'b': 'bbb'}
+        update = {'a': 'aaa', 'b': 'bbb'}
+        author_book.delete_book(example)
+        self.assertEqual(update, example)
+        self.assertTrue('Not Found' in all_output(mock_print))
 
 
     @patch('builtins.print')
